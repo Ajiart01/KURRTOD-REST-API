bd60852369b4bfdd7c86a333c04aac17be75e74e
Ajiart01 <139549928+Ajiart01@users.noreply.github.com>
2024-06-10 01:07:25 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'mongodb+srv://ajiart01:Aji11032001@cluster0.zebgfie.mongodb.net/?retryWrites=true&w=majority';
+var url = 'mongodb+srv://zeltoria1909:96xAwEIGoHhOO2lR@cluster0.v5c9etv.mongodb.net/?retryWrites=true&w=majority';
 try {
-if(url == 'mongodb+srv://ajiart01:Aji11032001@cluster0.zebgfie.mongodb.net/?retryWrites=true&w=majority') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'mongodb+srv://zeltoria1909:96xAwEIGoHhOO2lR@cluster0.v5c9etv.mongodb.net/?retryWrites=true&w=majority') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
